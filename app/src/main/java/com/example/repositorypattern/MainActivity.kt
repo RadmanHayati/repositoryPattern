f13e7fb1919ea8f99d1f0dc6f945e4248836f3c1
Radman Hayati <radman.hayatii@gmail.com>
2021-02-16 04:21:24 +0330

    ready to use!

--- app/src/main/java/com/example/repositorypattern/MainActivity.kt
+++ app/src/main/java/com/example/repositorypattern/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.repositorypattern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.repositorypattern.servicesandrepositories.repositories.UsersRepository
+import com.example.repositorypattern.servicesandrepositories.services.usersServices
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    var a:UsersRepository?=null
+        a=usersServices()
+        Toast.makeText(this, a.newStudent("sb","01234"), Toast.LENGTH_SHORT).show()
+
     }
 }
